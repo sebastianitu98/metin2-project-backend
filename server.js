4627744f5e8e35dee91ec7b31ab3dd0e57614303
Itu <sebastianitu61@yahoo.com>
2023-06-11 21:50:52 +0300

    'ADDED SSL CERTIFICATE'

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const platformsRoutes = require('./routes/platforms')
 const mongoose = require('mongoose')
 var cors = require('cors')
+const fs = require('fs')
+
+const file = fs.readFileSync('./B22F499C420CC2971E6C76E1966C65E9.txt')
 
 const app = express()
 app.use(cors())
@@ -17,6 +20,10 @@ app.use((req, res, next) => {
 })
 
 app.use(platformsRoutes)
+
+app.get('/.well-known/pki-validation/B22F499C420CC2971E6C76E1966C65E9.txt', (req, res) => {
+    res.sendFile('/home/ec2-user/metin2-project-backend/B22F499C420CC2971E6C76E1966C65E9.txt')
+})
 
 //connect do db
 mongoose.connect('mongodb+srv://sebastianitu61:guBCdD86trZgcqTC@metin2platforms.6zhlwhe.mongodb.net/?retryWrites=true&w=majority')
